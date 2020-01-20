d7a9f5eb338dafefe8b12e670b1a122be4a9a3e4
giatruong27 <giatruongtran27@gmail.com>
2020-01-20 14:14:13 +0700

    fix signup, login

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
 
 
-@staff_member_required
+@staff_member_required(login_url='account_login')
 def index(request):
     return render(request, 'dashboard/index.html')
