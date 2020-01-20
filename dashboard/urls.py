85c97ea8227cf4e16fa32a0b27013f7c7656acbf
giatruong27 <giatruongtran27@gmail.com>
2020-01-20 22:21:57 +0700

    finish course dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.conf.urls import url
 
 from .views import *
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path('', index, name='index'),
     path('courses/', CourseListView.as_view(), name='list_course'),
     path('courses/new/', CourseNewView.as_view(), name='new_course'),
+    url(r'^courses/edit/(?P<slug>[\w-]+)/$',
+        CourseEditView.as_view(), name='update_course'),
+    path('courses/<int:pk>/delete/',
+         CourseDeleteView.as_view(), name='delete_course'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,6 +7,9 @@ from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.contrib.auth.decorators import user_passes_test
 from courses.models import Course
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 
 
 @staff_member_required(login_url='account_login')
@@ -46,3 +49,15 @@ class CourseNewView(View):
             return redirect('dashboard:list_course')
 
         return render(request, self.template_name, context=context)
+
+
+class CourseEditView(UpdateView):
+    model = Course
+    template_name = 'dashboard/course/edit.html'
+    slug_field = 'slug'
+    form_class = course.CourseForm
+
+
+class CourseDeleteView(DeleteView):
+    model = Course
+    success_url = reverse_lazy('dashboard:list_course')
