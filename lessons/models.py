29b99e61f5672c07c6920948006973ab66459144
giatruong27 <giatruongtran27@gmail.com>
2020-01-22 11:34:19 +0700

    add lesson

---
+++ lessons/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from classes.models import YogaClass
+
+ACTIVE_STATE = 0
+INACTIVE_STATE = 1
+
+STATE_CHOICES = (
+    (ACTIVE_STATE, _('Active')),
+    (INACTIVE_STATE, _('Inactive')),
+)
+
+
+class Lesson(models.Model):
+    yogaclass = models.ForeignKey(
+        YogaClass, on_delete=models.CASCADE, related_name='classes')
+    state = models.IntegerField(choices=STATE_CHOICES,
+                                default=ACTIVE_STATE)
+    day = models.DateField(help_text=_('Day of the event'))
+    start_time = models.TimeField(help_text=_('Starting time'))
+    end_time = models.TimeField(help_text=_('Final time'))
+    notes = models.TextField(help_text=_('Notes'), blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
--- yoga_center_website/settings.py
+++ yoga_center_website/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'core',
     'courses',
     'classes',
+    'lessons',
     'dashboard',
 ]
 
