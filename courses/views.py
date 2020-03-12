0c3540e1618e6368597a7ded3a6d411283a04307
giatruong27 <giatruongtran27@gmail.com>
2020-03-12 21:47:49 +0700

    update course list

--- courses/views.py
+++ courses/views.py
@@ -12,9 +12,19 @@ class CourseListView(ListView):
     ordering = ['created_at']
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        context = super(CourseListView, self).get_context_data(**kwargs)
+        context['active_nav'] = 'classes'
+        return context
+
 
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/show.html'
     slug_field = 'slug'
     context_object_name = 'course'
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['active_nav'] = 'classes'
+        return context
