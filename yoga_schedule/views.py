59fdddf94516a1a57e532d723a5417d5b23d6eff
giatruong27 <giatruongtran27@gmail.com>
2020-03-13 21:14:20 +0700

    update schedule

--- yoga_schedule/views.py
+++ yoga_schedule/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rooms.models import Room
 
 def index(request):
-    context = {}
+    context = {
+        'active_nav': 'schedule'
+    }
     context['rooms'] =  Room.objects.all()
     return render(request, 'yoga_schedule/index.html', context=context)
