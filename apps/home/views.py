e0a599fcf64a73a735c35bf0a6a5ab76d729971a
giatruong27 <giatruongtran27@gmail.com>
2020-04-18 19:46:02 +0700

    update index page

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import redirect, render
+from apps.accounts.models import Trainer
 
 
 def home(request):
+    trainers = Trainer.objects.all()
     context = {
-        'active_nav': 'home'
+        'active_nav': 'home',
+        'trainers': trainers
     }
     return render(request, 'home/index.html', context=context)
