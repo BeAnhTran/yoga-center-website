aea00315752a025a945f8901faca532fceea0a17
giatruong27 <giatruongtran27@gmail.com>
2020-05-14 10:30:49 +0700

    update

--- apps/classes/serializers/yoga_class_serializer.py
+++ apps/classes/serializers/yoga_class_serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from apps.classes.models import YogaClass
 from apps.accounts.serializers.trainer_serializer import TrainerSerializer
+from apps.courses.serializers import CourseSerializer
 
 
 class YogaClassSerializer(serializers.ModelSerializer):
     trainer = TrainerSerializer()
+    course = CourseSerializer()
 
     class Meta:
         model = YogaClass
