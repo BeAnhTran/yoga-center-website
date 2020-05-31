1bbb487aca99eef482fad58cb0d0fc4a13a8ff16
giatruong27 <giatruongtran27@gmail.com>
2020-05-31 15:34:38 +0700

    add 500 error

--- apps/errors/urls.py
+++ apps/errors/urls.py
@@ -7,4 +7,5 @@ app_name = 'errors'
 urlpatterns = [
     path('403/', views._403, name='error-403'),
     path('404/', views._404, name='error-404'),
+    path('500/', views._500, name='error-500'),
 ]
--- apps/errors/views.py
+++ apps/errors/views.py
@@ -5,5 +5,10 @@ from django.http import HttpResponse
 def _403(request):
     return render(request, '403.html')
 
+
 def _404(request):
     return render(request, '404.html')
+
+
+def _500(request):
+    return render(request, '500.html')
