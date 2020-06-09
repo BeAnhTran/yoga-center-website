ccf51a16e5c30fc16dc5c530b8ca9ccefe36feaa
giatruong27 <giatruongtran27@gmail.com>
2020-06-09 13:35:15 +0700

    add payment period for class

--- apps/classes/models.py
+++ apps/classes/models.py
@@ -68,3 +68,15 @@ class YogaClass(models.Model):
         result = self.lessons.filter(
             date__range=[s, s + timedelta(days=6)]).order_by('date')
         return result
+
+
+class PaymentPeriod(models.Model):
+    yoga_class = models.ForeignKey(
+        YogaClass, on_delete=models.CASCADE, related_name='payment_periods', verbose_name=_('class'))
+    name = models.CharField(max_length=120, verbose_name=_('name'))
+    price = models.FloatField(verbose_name=_('price'))
+    end_at = models.DateField(blank=True, null=True, verbose_name=_('end at'))
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('created at'))
+    updated_at = models.DateTimeField(
+        auto_now=True, blank=True, null=True, verbose_name=_('updated at'))
