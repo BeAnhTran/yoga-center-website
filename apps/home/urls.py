46fd60d3780a068db249287ed45d98a3fb3e3cab
giatruong27 <giatruongtran27@gmail.com>
2020-07-22 22:49:05 +0700

    add AboutUs page

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -8,4 +8,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.HomeIndexView.as_view(), name='index'),
     path('contact/', views.ContactView.as_view(), name='contact'),
+    path('about-us/', views.AboutUsView.as_view(), name='about-us'),
 ]
