fc11775077f654d0a30420216dcb62388d1c5cd5
giatruong27 <giatruongtran27@gmail.com>
2020-07-22 17:13:41 +0700

    Update settings.py

--- yoga_center_website/settings.py
+++ yoga_center_website/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import dj_database_url
 import os
 from getenv import env
-from celery.schedules import crontab
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
